0cd843b695b6910017ca315a8c6f61fe3930ae80
Eryn Seo <yeryeon@gmail.com>
2022-02-19 13:28:48 +0900

    0083: Python

---
+++ leetcode/0083_RemoveDuplicatesFromSortedList.py
@@ -0,0 +1,20 @@
+# knowledge: X
+# concepts: linked list
+# comment: looping start and end is always the problem :/ almost got it though
+# runtime: X, not mine
+# memory usage: X, not mine
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def deleteDuplicates(self, head):
+        cur = head
+        while cur:
+            while cur.next and cur.next.val == cur.val:
+                cur.next = cur.next.next     # skip duplicated node
+            cur = cur.next     # not duplicate of current node, move to next node
+        return head
