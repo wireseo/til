be0400f8a6565d227e9a217f3dbe84f632d592d5
Eryn Seo <yeryeon@gmail.com>
2022-03-20 10:02:55 +0900

    0350: Python

---
+++ leetcode/0350_IntersectionOfTwoArraysII.py
@@ -0,0 +1,51 @@
+# knowledge: collections.Counter()
+# concepts: X 
+# comment: can sort first / use dictionary / use counter
+# runtime: X, not mine
+# memory usage: X, not mine
+
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        nums1, nums2 = sorted(nums1), sorted(nums2)
+        pt1 = pt2 = 0
+        res = []
+
+        while True:
+            try:
+                if nums1[pt1] > nums2[pt2]:
+                    pt2 += 1
+                elif nums1[pt1] < nums2[pt2]:
+                    pt1 += 1
+                else:
+                    res.append(nums1[pt1])
+                    pt1 += 1
+                    pt2 += 1
+            except IndexError:
+                break
+
+        return res
+
+    def intersect(self, nums1, nums2):
+        counts = {}
+        res = []
+
+        for num in nums1:
+            counts[num] = counts.get(num, 0) + 1
+
+        for num in nums2:
+            if num in counts and counts[num] > 0:
+                res.append(num)
+                counts[num] -= 1
+
+        return res
+
+    def intersect(self, nums1, nums2):
+        counts = collections.Counter(nums1)
+        res = []
+
+        for num in nums2:
+            if counts[num] > 0:
+                res += num,
+                counts[num] -= 1
+
+        return res
