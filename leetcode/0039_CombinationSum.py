a9226b8f97ceb57d326187b41e111631e067e4b0
Eryn Seo <yeryeon@gmail.com>
2022-04-11 12:04:31 +0900

    0039: Python

---
+++ leetcode/0039_CombinationSum.py
@@ -0,0 +1,20 @@
+# knowledge: X
+# concepts: backtracking
+# comment: dfs version; memorize template for backtracking questions
+# runtime: X, not mine
+# memory usage: X, not mine
+
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        ret = []
+        self.dfs(candidates, target, [], ret)
+        return ret
+    
+    def dfs(self, nums, target, path, ret):
+        if target < 0:
+            return 
+        if target == 0:
+            ret.append(path)
+            return 
+        for i in range(len(nums)):
+            self.dfs(nums[i:], target-nums[i], path+[nums[i]], ret)
