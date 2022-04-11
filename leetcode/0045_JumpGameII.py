0a268e600d6be929e84c7c341882adcaf46843e7
Eryn Seo <yeryeon@gmail.com>
2022-04-11 12:15:08 +0900

    0045_Python

---
+++ leetcode/0045_JumpGameII.py
@@ -0,0 +1,21 @@
+# knowledge: X
+# concepts: greedy
+# comment: difficult question, need to understand how to optimize
+# runtime: X, not mine
+# memory usage: X, not mine
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        if len(nums) <= 1: 
+            return 0
+        
+        l, r = 0, nums[0]
+        times = 1
+        
+        while r < len(nums) - 1:
+            times += 1
+            nxt = max(i + nums[i] for i in range(l, r + 1))
+            l, r = r, nxt
+            
+        return times
+    
