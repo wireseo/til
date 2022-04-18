16b2be3d275fb31ab7c2a677a9e3e01f1bf090ab
Eryn Seo <yeryeon@gmail.com>
2022-04-18 09:18:26 +0900

    0661: Python

---
+++ leetcode/0661_ImageSmoother.py
@@ -0,0 +1,18 @@
+# knowledge: row, col = len(M), len(M[0]); add to temp if 0<=i+m<row and 0<=j+n<col
+# concepts: matrix
+# comment: create dirs array
+# runtime: X, not mine
+# memory usage: X, not mine
+
+class Solution:
+    def imageSmoother(self, M: List[List[int]]) -> List[List[int]]:
+        row, col = len(M), len(M[0])
+        res = [[0]*col for i in range(row)]
+        dirs = [[0,0],[0,1],[0,-1],[1,0],[-1,0],[1,1],[-1,-1],[-1,1],[1,-1]]
+        
+        for i in range(row):
+            for j in range(col):
+                temp = [M[i+m][j+n] for m,n in dirs if 0<=i+m<row and 0<=j+n<col]
+                res[i][j] = sum(temp)//len(temp)
+                
+        return res
