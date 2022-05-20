dd61b11b1eb847559af6deb92941e2ebe15a694e
Eryn Seo <yeryeon@gmail.com>
2022-05-20 21:10:59 +0900

    0258: Python

---
+++ leetcode/0258_AddDigits.py
@@ -0,0 +1,13 @@
+# knowledge: X
+# concepts: num sum
+# comment: mathematical theorem needed
+# runtime: X, not mine
+# memory usage: X, not mine
+
+class Solution:
+    def addDigits(self, num: int) -> int:
+        if num == 0:
+            return 0
+        if num % 9 == 0:
+            return 9
+        return num % 9
