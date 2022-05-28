484e6c53995fb272f226cfb124662dd0a0e297fd
Eryn Seo <yeryeon@gmail.com>
2022-05-28 13:07:49 +0900

    0747: Python

---
+++ leetcode/0747_LargestNumberAtLeastTwiceOfOthers.py
@@ -0,0 +1,16 @@
+# knowledge: X
+# concepts: highest and second highest
+# comment: X
+# runtime: X, not mine
+# memory usage: X, not mine
+
+class Solution:
+    def dominantIndex(self, nums: List[int]) -> int:
+        max1 = max2 = maxidx = -1 
+        
+        for idx, num in enumerate(nums):
+            if num > max1:
+                max2, max1, maxidx  = max1, num, idx
+            elif num > max2:
+                max2 = num
+        return maxidx if max1 >= 2 * max2 else -1
