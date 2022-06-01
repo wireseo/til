a54c56c996076e0ec28d8bd346d1454fd11f6e26
Eryn Seo <yeryeon@gmail.com>
2022-06-01 15:16:07 +0900

    0049: Python

---
+++ leetcode/0049_GroupAnagrams.py
@@ -0,0 +1,13 @@
+# knowledge: list(d.values())
+# concepts: dictionary
+# comment: think of using a dictionary when seeing something that can be "grouped" into a same key
+# runtime: X, not mine
+# memory usage: X, not mine
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        d = {}
+        for word in strs:
+            key = tuple(sorted(word))
+            d[key] = d.get(key, []) + [word]
+        return list(d.values())
