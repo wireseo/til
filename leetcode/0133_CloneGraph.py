033779ad8c61eeff2743f7ab05066b4d02f3101d
Eryn Seo <yeryeon@gmail.com>
2022-06-26 23:18:01 +0900

    0133: Python

---
+++ leetcode/0133_CloneGraph.py
@@ -0,0 +1,32 @@
+# knowledge: X
+# concepts: BFS, Graph
+# comment: need to practice graph problems
+# runtime: X, not mine
+# memory usage: X, not mine
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if not node:
+            return node
+        
+        q, clones = deque([node]), {node.val: Node(node.val, [])}
+        while q:
+            cur = q.popleft() 
+            cur_clone = clones[cur.val]            
+
+            for neighbor in cur.neighbors:
+                if neighbor.val not in clones:
+                    clones[neighbor.val] = Node(neighbor.val, [])
+                    q.append(neighbor)
+                    
+                cur_clone.neighbors.append(clones[neighbor.val])
+                
+        return clones[node.val]
