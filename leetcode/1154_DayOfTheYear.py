7c163e60312e7cb02732abc7eae52af253c73fed
Eryn Seo <yeryeon@gmail.com>
2022-06-28 23:32:23 +0900

    1154: Python

---
+++ leetcode/1154_DayOfTheYear.py
@@ -0,0 +1,15 @@
+# knowledge: X
+# concepts: String manipulation
+# comment: easy question, but need elegant solution
+# runtime: X, not mine
+# memory usage: X, not mine
+
+class Solution:
+    def dayOfYear(self, date: str) -> int:
+        y, m, d = map(int, date.split('-'))
+        days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+        
+        if (y % 400) == 0 or ((y % 4 == 0) and (y % 100 != 0)): 
+            days[1] = 29
+            
+        return d + sum(days[:m-1])
