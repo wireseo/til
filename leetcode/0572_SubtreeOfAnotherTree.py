499c3673b3f7af68bcbb19b109eb9793f1b89378
Eryn Seo <yeryeon@gmail.com>
2022-06-29 10:19:02 +0900

    0572: Python

---
+++ leetcode/0572_SubtreeOfAnotherTree.py
@@ -0,0 +1,25 @@
+# knowledge: X
+# concepts: recursion, BST
+# comment: can recurse twice (in helper method and in outer method)
+# runtime: X, not mine
+# memory usage: X, not mine
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isSubtree(self, s: Optional[TreeNode], t: Optional[TreeNode]) -> bool:
+        def recurse(s, t):
+            if not (s and t):
+                return s is t
+            return (s.val == t.val and recurse(s.left, t.left) and recurse(s.right, t.right))
+            
+        if recurse(s, t): 
+            return True
+        if not s: 
+            return False
+        return self.isSubtree(s.left, t) or self.isSubtree(s.right, t)
