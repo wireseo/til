a1c7bfb106ee5e0509059c1cde20abb201bdcc8e
Eryn Seo <yeryeon@gmail.com>
2022-08-08 14:06:43 +0800

    0036: Python

---
+++ leetcode/0036_ValidSudoku_Other.py
@@ -0,0 +1,12 @@
+def isValidSudoku(self, board):
+    seen = sum(([(c, i), (j, c), (i/3, j/3, c)]
+                for i, row in enumerate(board)
+                for j, c in enumerate(row)
+                if c != '.'), [])
+    return len(seen) == len(set(seen))
+
+def isValidSudoku(self, board):
+    seen = sum(([(c, i), (j, c), (i/3, j/3, c)]
+                for i in range(9) for j in range(9)
+                for c in [board[i][j]] if c != '.'), [])
+    return len(seen) == len(set(seen))
