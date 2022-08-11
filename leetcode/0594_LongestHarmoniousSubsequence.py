1229402718645b6992046c8411647ba0afb06ea2
Eryn Seo <yeryeon@gmail.com>
2022-08-11 13:59:14 +0800

    0594: Python

---
+++ leetcode/0594_LongestHarmoniousSubsequence.py
@@ -0,0 +1,10 @@
+# knowledge: X
+# concepts: counter and list manipulation
+# comment: need to know how to optimize problems, do it before solving them
+# runtime: X, not mine
+# memory usage: X, not mine
+
+class Solution:
+    def findLHS(self, nums: List[int]) -> int:
+        c = collections.Counter(nums)
+        return max([c[x] + c[x + 1] for x in c if x + 1 in c] or [0])
